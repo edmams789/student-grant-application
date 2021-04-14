8ab1fd8e9918dad6774340cd5a110d606e47d8be
Alexander <edmams789@gmail.com>
2021-04-14 15:00:25 +0300

    Сохраняем данные - пакетное исполнение - item 50

--- src/main/java/com/borisov/subsidyApplication/dao/StudentOrderDaoImpl.java
+++ src/main/java/com/borisov/subsidyApplication/dao/StudentOrderDaoImpl.java
@@ -121,11 +121,21 @@ public class StudentOrderDaoImpl implements StudentOrderDao{
     private void saveChildren(Connection con, StudentOrder so, Long soId) throws SQLException{
         
         try (PreparedStatement stmt = con.prepareStatement(INSERT_CHILD)) {
+//            int counter = 0;
             for (Child child : so.getChildren()) {
                 stmt.setLong(1, soId);
                 setParamsForChild(stmt, child);
-                stmt.executeUpdate();
-            }           
+//                stmt.executeUpdate();
+                stmt.addBatch(); //добавл. команду в пакет котор. копит данные 
+//                counter++;
+//                if (counter > 10000) {
+//                    stmt.executeBatch();
+//                    counter = 0;
+//                }
+//            }        
+//            if (counter > 0) {
+                stmt.executeBatch(); //исполняет данные накопленные в пакете 
+            }            
         }
     }
         
